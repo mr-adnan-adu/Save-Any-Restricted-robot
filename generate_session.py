60e7f5edde863b1cb758d4477e11c64dbc8ebff3
MUHAMMED ADNAN <73103641+mr-adnan-adu@users.noreply.github.com>
2025-05-25 09:55:34 +0530

    Update generate_session.py

--- generate_session.py
+++ generate_session.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
-api_id = int(input("Enter your API_ID: "))
-api_hash = input("Enter your API_HASH: ")
+api_id = int(input("Enter your API_ID: 8078347 "))
+api_hash = input("Enter your API_HASH: 721e258069f64e1ecb75c56907927ff2")
 
 with Client("userbot", api_id, api_hash) as app:
     print("Session string:", app.export_session_string())
